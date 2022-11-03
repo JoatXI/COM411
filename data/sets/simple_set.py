f084afc76af41ea1e9b5c86ff1457ac92b2b4d78
Michael Paterson <michaelpaterson194@gmail.com>
2022-11-03 12:48:29 +0000

    slow learning

---
+++ data/sets/simple_set.py
@@ -0,0 +1,8 @@
+def observed():
+    observations = {"Flying cars", "Sky scrapper", "Sky scrapper", "Laser", "Dome", "Dome"}
+    print(observations)
+    
+def run(show):
+    show()
+    
+run(observed)
