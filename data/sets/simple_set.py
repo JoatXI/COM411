babd193513d3f67cf4113ae5795b8a9a7cc085b1
Michael Paterson <michaelpaterson194@gmail.com>
2022-11-08 13:50:33 +0000

    _

--- data/sets/simple_set.py
+++ data/sets/simple_set.py
@@ -1,6 +1,6 @@
 def observed():
     observations = {"Flying cars", "Sky scrapper", "Sky scrapper", "Laser", "Dome", "Dome"}
-    print(observations)
+    return observations
     
 def run():
     print(observed())
