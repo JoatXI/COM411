b8b955bc19aff5d78a14115dba5a5d9172ba376a
Michael Paterson <michaelpaterson194@gmail.com>
2022-11-10 12:49:03 +0000

    _

---
+++ visual/plots/squares.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def small():
+    x = [3, 4]
+    y = [3, 4]
+    
+    plt.plot(x, y, 's')
+    plt.plot(x, y, 'ro:')
+    plt.show()
+    
+def medium():
+    x = [2, 5]
+    y = [2, 5]
